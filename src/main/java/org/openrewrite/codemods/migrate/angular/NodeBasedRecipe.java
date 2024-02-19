cf22e328a296e7a651859b8b6f21f530948dda1a
Scott Jungling <scott@moderne.io>
2024-02-19 11:02:28 -0800

    chore: force install node-gyp and nan locally

--- src/main/java/org/openrewrite/codemods/migrate/angular/NodeBasedRecipe.java
+++ src/main/java/org/openrewrite/codemods/migrate/angular/NodeBasedRecipe.java
@@ -124,8 +124,8 @@ public abstract class NodeBasedRecipe extends ScanningRecipe<NodeBasedRecipe.Acc
             }
             
             // Install node-gyp globally to avoid issues with `npx`
-            runCommand(Arrays.asList("npm", "install", "--save-dev","nan"), dir, nodeModules, ctx);
-            runCommand(Arrays.asList("npm", "install", "--save-dev","node-gyp@latest"), dir, nodeModules, ctx);
+            runCommand(Arrays.asList("npm", "install", "--force", "--save-dev","nan"), dir, nodeModules, ctx);
+            runCommand(Arrays.asList("npm", "install", "--force", "--save-dev","node-gyp@latest"), dir, nodeModules, ctx);
 
             runCommand(npmInstallCommand, dir, nodeModules, ctx);
             Path out = runCommand(command, dir, nodeModules, ctx);
